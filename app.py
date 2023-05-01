79e378ef37155d2104cd71d0a363e82f06d2b5ab
Anderson Portes do Nascimento <portsanderson@gmail.com>
2023-05-01 17:56:59 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+musics_dict = pickle.load(open('data.pkl', 'rb'))
+musics = pd.DataFrame(musics_dict)
+
+
+def recommend(movie):
+    global musics, similarity
+    index = musics[musics['title'] == movie].index[0]
+    distances = similarity[index]
+    musics_list = sorted(list(enumerate(distances)),
+                         reverse=True, key=lambda x: x[1])[1:6]
+    recommended_musics = []
+    for i in musics_list:
+        recommended_musics.append(musics.iloc[i[0]].title)
+    return recommended_musics
+
+
+st.title('Music Recommender System')
+selected_music = st.selectbox(
+    'Select the music',
+    musics['title'].values
+)
+
+if st.button('Recommend'):
+    recommendations = recommend(selected_music)
+    for i in recommendations:
+        st.write(i)
